da3cce2be4e45807567808391becaf0d2c18b596
sugita <sugita5019@gmail.com>
2019-07-09 15:18:22 +0800

    new loop test

---
+++ py-loop-alltype.py
@@ -0,0 +1,30 @@
+shopping_list = [] #initate empty list first
+
+groceries = ["milk",
+             "eggs",
+             "curry powder",
+             "coffee",
+             "instant noodles"] #list of groceries
+
+tools = ["hammer",
+         "nails",
+         "screw",
+         "bulb"] #list of tools required
+
+babyitems = ["diapers",
+             "wet tissue",
+             "baby powder",
+             "socks"] # baby's
+
+
+shopping_list.append(groceries)
+shopping_list.append(tools)
+shopping_list.append(babyitems)
+
+#loop printing item
+for item in shopping_list:
+    print(item)
+
+
+
+
