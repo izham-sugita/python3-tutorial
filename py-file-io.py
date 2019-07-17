5f0741da43418cd02ba676a7423348251ee7e1e8
sugita <sugita5019@gmail.com>
2019-07-17 12:01:28 +0800

    Updated

--- py-file-io.py
+++ py-file-io.py
@@ -1,3 +1,13 @@
 fo = open("test.txt","w+")
 fo.write("Test file\n")
 fo.close()
+
+fo = open("test.txt","a+")
+anim_list = ["cat","tiger","bear","elephant","mouse","dear"]
+
+for animal in anim_list:
+    print(animal)
+    fo.write(animal+"\n")
+
+fo.close()
+    
