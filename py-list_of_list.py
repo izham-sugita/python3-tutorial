704565452d0129de4510730807725ec8f2db21a2
sugita <sugita5019@gmail.com>
2019-07-18 16:08:25 +0800

    updated

--- py-list_of_list.py
+++ py-list_of_list.py
@@ -1,23 +1,37 @@
+import numpy as np
+
+pi = np.pi
 #create list of list
 list1D = []
 
+imax = 11
+jmax = 11
+dx = 1.0/(imax-1)
+dy = dx
+
 for i in range(imax):
-    list1D.append(float(i))
-    
-mylist = []
+    list1D.append(0.0)
+
+list2D = []
 for j in range(jmax):
-    mylist.append(list1D)
+    list2D.append([0.0 for i in range(imax)])
+    #list2D.append(list1D) <-cannot be used because object list1D needs
+    #to be changed; not from index i j only
 
-for i in range(imax):
-    for j in range(imax):
-        #print(mylist[i][j])
-        mylist[i][j] = 0.0
+print(list2D)
 
-for i in range(imax):
-    for j in range(jmax):
-        #print(mylist[i][j])
-        mylist[i][j] = m.sin(i*dx*pi)*m.sin(j*dy*pi)
+print()
 
 for i in range(imax):
     for j in range(jmax):
-        print(mylist[i][j])
+        list2D[i][j] = np.sin(i*dx*pi)*np.sin(j*dy*pi) #i*jmax + j
+        #print(list2D[i][j])
+
+xc = int(imax/2)
+yc = int(jmax/2)
+print(list2D[xc][yc])
+
+
+
+
+
