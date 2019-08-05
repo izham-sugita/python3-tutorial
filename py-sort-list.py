af58af7b3e3a6bedfa3efe769c5bd47e03c8e7a2
sugita <sugita5019@gmail.com>
2019-08-05 16:00:47 +0800

    updated

---
+++ py-sort-list.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+survey = pd.read_csv("survey.csv")
+register = pd.read_csv("register.csv")
+
+#print(survey.index)
+#print(register.index)
+
+survey_Col = survey.columns
+survey_Val = survey.values
+
+#print(survey_Col)
+#print(survey_Val)
+
+
+for str in survey_Col:
+    if str == 'Username':
+        email = str
+
+#print(email)
+#print(survey[email])
+
+elistsurvey = survey[email].tolist()
+elistregistered = register[email].tolist()
+#print(len(elistregistered))
+#print(len(elistsurvey))
+
+totalreg = len(elistregistered)
+totalsur = len(elistsurvey)
+print("Total email in list ", totalreg+totalsur)
+
+dup = []
+for n1 in elistregistered:
+    for n2 in elistsurvey:
+        if n1 == n2:
+            #print(n2)
+            dup.append(n2)
+
+totaldup = len(dup)
+            
+for nc in dup:
+    for n1 in elistregistered:
+        if nc == n1:
+            elistregistered.remove(n1)
+
+cleanreg = len(elistregistered)
+            
+for nc in dup:
+    for n2 in elistsurvey:
+        if nc == n2:
+            elistsurvey.remove(n2)
+
+cleansur = len(elistsurvey)
+            
+#print(totaldup)
+#print(cleanreg)
+#print(cleansur)
+#print("Total email without dups ", totaldup+cleanreg+cleansur)
+
+for n1 in elistregistered:
+    dup.append(n1)
+
+for n2 in elistsurvey:
+    dup.append(n2)
+
+finalcount = len(dup)
+print("Final email list without dups ", finalcount)
+
+for nc in dup:
+    print(nc)
+
+f = open("mail-list.txt","w")
+for nc in dup:
+    f.write(nc)
+    f.write(";\n")
+
+f.close()
+    
